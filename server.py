8e22a261b8cf02c304c77a8203c52b7b0bdfe6aa
errornesttorn <146091620+errornesttorn@users.noreply.github.com>
2023-10-20 11:51:07 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -12,9 +12,9 @@ OPENAIAPIKEY += input("Input the THEPASS: ")
 
 openai.api_key = OPENAIAPIKEY
 
-def completion_api(prompt: str, engine: str="gpt-3.5-turbo-instruct",\
-                    temperature:float | int=0.0, max_new_tokens: int=1024,\
-                    stop: list | None=None) -> str:
+def completion_api(prompt, engine: str="gpt-3.5-turbo-instruct",\
+                    temperature=0.0, max_new_tokens: int=1024,\
+                    stop=None) -> str:
     try:
         komplicja = openai.Completion.create(
             engine=engine,
